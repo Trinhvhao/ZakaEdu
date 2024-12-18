339dc4818cdb4ae6e76a4ddf54eb8aa3821167d0
Trinhvhao <haotrinh142@gmail.com>
2024-12-18 22:05:53 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,163 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner(0);
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 45) {
+      $(".nav-bar").addClass("sticky-top shadow-sm").css("top", "0px");
+    } else {
+      $(".nav-bar").removeClass("sticky-top shadow-sm").css("top", "-100px");
+    }
+  });
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    animateOut: "fadeOut",
+    items: 1,
+    margin: 0,
+    stagePadding: 0,
+    autoplay: true,
+    smartSpeed: 500,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+  });
+
+  // testimonial carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    center: false,
+    dots: false,
+    loop: true,
+    margin: 25,
+    nav: true,
+    navText: [
+      '<i class="fa fa-arrow-right"></i>',
+      '<i class="fa fa-arrow-left"></i>',
+    ],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 2,
+      },
+      1200: {
+        items: 2,
+      },
+    },
+  });
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 5,
+    time: 2000,
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+})(jQuery);
+
+// Mockup
+/**
+ * Initiate glightbox
+ */
+const glightbox = GLightbox({
+  selector: ".glightbox",
+});
+/**
+ * Animation on scroll function and init
+ */
+
+/**
+ * Init swiper sliders
+ */
+function initSwiper() {
+  document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
+    let config = JSON.parse(
+      swiperElement.querySelector(".swiper-config").innerHTML.trim()
+    );
+
+    if (swiperElement.classList.contains("swiper-tab")) {
+      initSwiperWithCustomPagination(swiperElement, config);
+    } else {
+      new Swiper(swiperElement, config);
+    }
+  });
+}
+
+window.addEventListener("load", initSwiper);
+
+/*** Init isotope layout and filters
+ */
+document.querySelectorAll(".isotope-layout").forEach(function (isotopeItem) {
+  let layout = isotopeItem.getAttribute("data-layout") ?? "masonry";
+  let filter = isotopeItem.getAttribute("data-default-filter") ?? "*";
+  let sort = isotopeItem.getAttribute("data-sort") ?? "original-order";
+
+  let initIsotope;
+  imagesLoaded(isotopeItem.querySelector(".isotope-container"), function () {
+    initIsotope = new Isotope(isotopeItem.querySelector(".isotope-container"), {
+      itemSelector: ".isotope-item",
+      layoutMode: layout,
+      filter: filter,
+      sortBy: sort,
+    });
+  });
+
+  isotopeItem
+    .querySelectorAll(".isotope-filters li")
+    .forEach(function (filters) {
+      filters.addEventListener(
+        "click",
+        function () {
+          isotopeItem
+            .querySelector(".isotope-filters .filter-active")
+            .classList.remove("filter-active");
+          this.classList.add("filter-active");
+          initIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          if (typeof aosInit === "function") {
+            aosInit();
+          }
+        },
+        false
+      );
+    });
+});
